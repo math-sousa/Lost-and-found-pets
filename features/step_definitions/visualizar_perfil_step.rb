fff7fcaa64182457a3ad0aba0ad8db00f957de0e
Raquel Almeida <raquelcristianealm@gmail.com>
2022-10-09 14:40:44 -0300

    test: Add steps for view profile

---
+++ features/step_definitions/visualizar_perfil_step.rb
@@ -0,0 +1,29 @@
+Dado('que estou no mural de posts') do
+    pending # Write code here that turns the phrase above into concrete 
+    actions
+end
+
+Quando('clico em perfil') do
+    pending # Write code here that turns the phrase above into concrete 
+    actions
+end
+
+Então('deverei ver a página de título {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete 
+    actions
+end
+
+Dado('um usuário cadastrado {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete 
+    actions
+end
+
+Dado('que estou na página meu perfil') do
+    pending # Write code here that turns the phrase above into concrete 
+    actions
+end
+
+Então('o campo {string} deve estar preenchido com {string}') do |string, string2|
+    pending # Write code here that turns the phrase above into concrete 
+    actions
+end
