9a939eff81d4e5fe924f26105fd5417ed276cb89
Artur Cupelli <artur.cupelli@usp.br>
2022-10-12 16:52:25 -0300

    feat/test: Add error messages

--- app/controllers/perfil_controller.rb
+++ app/controllers/perfil_controller.rb
@@ -1,10 +1,15 @@
 class PerfilController < ApplicationController
     def new
+      @perfil = Perfil.new
     end
+
     def create
         @perfil = Perfil.new(perfil_params) 
         if @perfil.save
-            redirect_to '/perfil/'
+            redirect_to @perfil
+        else
+          render :new, status: :unprocessable_entity, content_type: "text/html"
+          headers["Content-Type"] = "text/hmtl"
       end
     end
     
--- app/models/perfil.rb
+++ app/models/perfil.rb
@@ -1,6 +1,6 @@
 class Perfil < ApplicationRecord
-    validates :nome, presence: {message: "O nome é obrigatório!"}
-    validates :email, presence: {message: "O email é obrigatório!"}, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Formato de e-mail inválido!" } 
-    validates :senha, presence: {message: "A senha é obrigatória!"}
-    validates :telefone, numericality: { only_integer: true }
+    validates :nome, presence: {message: "É obrigatório informar o nome!"}
+    validates :email, presence: {message: "É obrigatório informar o email!"}, format: { with: URI::MailTo::EMAIL_REGEXP, message: "O email deve ser válido!" } 
+    validates :senha, presence: {message: "É obrigatório informar a senha!"}
+    validates :telefone, presence: {message: "É obrigatório informar o telefone!"}, numericality: { only_integer: true, message: "O telefone deve ser válido!" }
 end
