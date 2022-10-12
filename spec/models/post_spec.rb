ca402627f693ded8be513255cc6a8b23af8839d0
Luan Monteiro <luancorreamonteiro@gmail.com>
2022-10-12 17:48:41 -0300

    test: added unit test for the model

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'invalido sem titulo' do
+    post = Post.new
+    post.descricao = "Alguma descricao."
+    expect(post).not_to be_valid
+  end
+
+  it 'invalido sem descrição' do
+    post = Post.new
+    post.titulo = "Algum titulo."
+    expect(post).not_to be_valid
+  end
+
+  it 'invalido - titulo com formato invalido' do
+    post = Post.new
+    post.titulo = "∞ ♫ ♬ ♭ ♮ ♯ ♰ ♱ ✁ ✂ ✃ ✄ ✆ ✇ ✈ ✉ ✌ ✍ ✎ ✏ ✐ ✑ ✒ ✓ ✔ ✕ ✖ ✗ ✘ ✙ ✚ ✛ ✜ ✝ ✞ ✟ ✠ ✡ ✢ ✣ ✤ ✥ ✦ ✧ ✩ ✪ ✫ ✬ ✭ ✮ ✯ ✰ ✱"
+    post.descricao = "Alguma descricao."   
+    expect(post).not_to be_valid
+  end
+
+  it 'invalido - descrição com formato invalido' do
+    post = Post.new
+    post.titulo = "Algum titulo."
+    post.descricao = "∞ ♫ ♬ ♭ ♮ ♯ ♰ ♱ ✁ ✂ ✃ ✄ ✆ ✇ ✈ ✉ ✌ ✍ ✎ ✏ ✐ ✑ ✒ ✓ ✔ ✕ ✖ ✗ ✘ ✙ ✚ ✛ ✜ ✝ ✞ ✟ ✠ ✡ ✢ ✣ ✤ ✥ ✦ ✧ ✩ ✪ ✫ ✬ ✭ ✮ ✯ ✰ ✱"   
+    expect(post).not_to be_valid
+  end
+end
+
