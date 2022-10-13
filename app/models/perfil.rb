769cf3aae4e6be25c100c4458287a064c9eefb9b
Davidson Nilson <davidson.nilson@nubank.com.br>
2022-10-12 22:18:42 -0300

    test: add authenticate function and change the class extends

--- app/models/perfil.rb
+++ app/models/perfil.rb
@@ -1,6 +1,15 @@
-class Perfil < ApplicationRecord
+class Perfil < ActiveRecord::Base
     validates :nome, presence: {message: "O nome é obrigatório!"}
     validates :email, presence: {message: "O email é obrigatório!"}, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Formato de e-mail inválido!" } 
     validates :senha, presence: {message: "A senha é obrigatória!"}
     validates :telefone, numericality: { only_integer: true }
+
+    def self.authenticate(email, senha)
+        perfil = find_by_email(email)
+        if(perfil && perfil.senha == senha)
+            perfil
+        else
+            nil
+        end
+    end
 end
