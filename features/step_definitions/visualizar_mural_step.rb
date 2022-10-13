a5e9466d493b0f3b5d35f5e20149ff69444717b9
Matheus Sousa <math.rmsousa@gmail.com>
2022-10-12 23:21:09 -0300

    Corrige mural

--- features/step_definitions/visualizar_mural_step.rb
+++ features/step_definitions/visualizar_mural_step.rb
@@ -1,27 +1,26 @@
+require 'database_cleaner/active_record'
+
 Dado('que estou na página de mural') do
     visit '/mural/index'
   end
   
-  Quando('não existem posts cadastrados') do
-    expect(page).to have_content('Não há posts para serem mostrados por enquanto.')
+  Dado('que não existem posts cadastrados') do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean 
   end
   
-  Então('deverei ver a mensagem de erro {string}') do |string|
-    expect(page).to have_content(string)
-  end
-
   Então('deverei ver o post na página do mural') do
-    expect(page).to eq("Cachorro teste1")
-    expect(page).to eq("teste1")
+    expect(page).to have_content("Cachorro YorkShire perdido em 10/08")
+    expect(page).to have_content("Cachorro atende pelo nome de 'Lulu'. Desapareceu no bairro dos laranjais, próximo a Rua Vale do Rio Doce.")
   end
 
-  Quando('existem posts cadastrados') do
-    expect(page).to have_content('Título')
-    expect(page).to have_content('Descrição')
+  Quando("existem posts cadastrados") do
+    @post = Post.new(:titulo=>'Cachorro YorkShire perdido em 10/08', :descricao=>"Cachorro atende pelo nome de 'Lulu'. Desapareceu no bairro dos laranjais, próximo a Rua Vale do Rio Doce.")
+    @post.save
   end
   
   Então('eu devo visualizar o último post cadastrado no banco de dados contendo titulo {string} e descricao {string}') do |string, string2|
-    posts = Post.order("id").last
+    post = Post.order("id").last
     expect(post.titulo).to eq(string)
     expect(post.descricao).to eq(string2)
   end
--- features/visualizar_mural.feature
+++ features/visualizar_mural.feature
@@ -7,13 +7,13 @@ Quero poder acessar o mural de posts
 
 Cenário: Não existem posts no mural
 Dado que estou na página de mural
-Quando não existem posts cadastrados
-Então deverei ver a mensagem de erro "Nenhum post cadastrado por enquanto"
+E que não existem posts cadastrados
+Então deverei ver a mensagem de erro "Nenhum post cadastrado por enquanto."
 
 Cenário: Cadastro de post com sucesso
 Dado que estou na página de cadastro de post
-Quando preencho o campo "Titulo" com "Cachorro teste1"
-Quando preencho o campo "Descricao" com "teste1"
+Quando preencho o campo "Titulo" com "Cachorro YorkShire perdido em 10/08"
+Quando preencho o campo "Descricao" com "Cachorro atende pelo nome de 'Lulu'. Desapareceu no bairro dos laranjais, próximo a Rua Vale do Rio Doce."
 E clico em criar
 Então ele deve ter sido salvo no banco de dados
 E deverei ver o post na página do mural
@@ -21,4 +21,4 @@ E deverei ver o post na página do mural
 Cenário: Visualizar o ultimo post com sucesso
 Dado que estou na página de mural
 Quando existem posts cadastrados
-Então eu devo visualizar o último post cadastrado no banco de dados contendo titulo "Cachorro teste1" e descricao "teste1"
+Então eu devo visualizar o último post cadastrado no banco de dados contendo titulo "Cachorro YorkShire perdido em 10/08" e descricao "Cachorro atende pelo nome de 'Lulu'. Desapareceu no bairro dos laranjais, próximo a Rua Vale do Rio Doce."
