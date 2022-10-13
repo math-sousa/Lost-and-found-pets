2ff468fe334bba10f36bb77fa5094de94ac85caa
Artur Cupelli <artur.cupelli@usp.br>
2022-10-12 23:38:24 -0300

    fix: Remove ambiguos tests case

--- features/step_definitions/cadastrar_perfil_step.rb
+++ features/step_definitions/cadastrar_perfil_step.rb
@@ -3,15 +3,11 @@ Dado('que estou na página de cadastro de usuário') do
     visit '/perfil/new'
 end
 
-Quando('preencho o campo {string} com {string}') do |string, string2|
-    fill_in string, :with => string2
-end
-
 Quando('clico em salvar') do
     click_on 'Salvar'
 end
 
-Então('ele deve ter sido salvo no banco de dados') do
+Então('o perfil deve ter sido salvo no banco de dados') do
     perfil = Perfil.order("id").last
     expect(perfil.nome).to eq('Maria Silva')
     expect(perfil.email).to eq('maria.silva@gmail.com') 
@@ -25,10 +21,6 @@ end
 
 Quando('deixo o campo {string} vazio') do |string|
     fill_in string, :with => ""
-end
-
-Então('deverei ver a mensagem de erro {string}') do |string|
-    expect(page).to have_content(string)
 end
 
 Dado('um usuário criado com o email {string}') do |string|
