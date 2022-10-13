7e68a8c1d1bb2a7f883469a036ed18d02f6a56cf
Davidson Nilson <davidson.nilson@nubank.com.br>
2022-10-12 22:20:44 -0300

    add login routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "application#index"
   resources :perfil
   get '/login', to: 'perfil#login'
-
+  post '/login', to: 'perfil#process_login'
 
   resources :posts
 end
