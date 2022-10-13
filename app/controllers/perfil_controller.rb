c5a0c5d6ddcbafc5f238c0a5877e39a257896e6c
Davidson Nilson <davidson.nilson@nubank.com.br>
2022-10-12 22:16:28 -0300

    test: add login controllers

--- app/controllers/perfil_controller.rb
+++ app/controllers/perfil_controller.rb
@@ -8,6 +8,25 @@ class PerfilController < ApplicationController
       end
     end
     def login
+      
+    end
+    def process_login
+      perfil = Perfil.authenticate(params[:email], params[:senha])
+      if(perfil)
+        session[:user_id]=perfil.id
+        redirect_to '/posts', :notice=>'logado'
+      else
+        flash.now.alert = 'Email e/ou senha inválidos'
+        render 'login'
+      end
+    end
+
+    def meu_perfil
+      if(session[:user_id]!=nil)
+        @sessionName = perfil.find(session[:user_id]).nome
+      else
+        @sessionName = 'convidade'
+      end
     end
     
       private
