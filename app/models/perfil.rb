ed734ec86cb3a7fdbc87759e4cf71347453ae760
Luan Monteiro <luan.monteiro@spacesheepgames.com>
2022-10-18 13:59:09 -0300

    feat: edit perfil model to use devise authentication

--- app/models/perfil.rb
+++ app/models/perfil.rb
@@ -1,15 +1,10 @@
 class Perfil < ActiveRecord::Base
-    validates :nome, presence: {message: "É obrigatório informar o nome!"}
-    validates :email, presence: {message: "É obrigatório informar o email!"}, format: { with: URI::MailTo::EMAIL_REGEXP, message: "O email deve ser válido!" }, uniqueness: {message: "Email já cadastrado!"} 
-    validates :senha, presence: {message: "É obrigatório informar a senha!"}
-    validates :telefone, presence: {message: "É obrigatório informar o telefone!"}, numericality: { only_integer: true, message: "O telefone deve ser válido!" }
-
-    def self.authenticate(email, senha)
-        perfil = find_by_email(email)
-        if(perfil && perfil.senha == senha)
-            perfil
-        else
-            nil
-        end
-    end
+    # Include default devise modules. Others available are:
+    # :lockable, :timeoutable, :trackable and  :omniauthable
+    devise :database_authenticatable, :registerable,
+            :recoverable, :rememberable, :validatable
+    validates :name, presence: true
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true 
+    validates :phone, presence: true, format: { without: /[a-zA-Z]/}
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
-  root to: "application#index"
-  resources :perfil
-  get '/login', to: 'perfil#login'
-  post '/login', to: 'perfil#process_login'
+  devise_for :perfils
+
+  devise_scope :perfil do
+    get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
+
+    authenticated :perfil do
+      root 'mural#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+    
+  end
 
   get '/mural/index', to: 'mural#index' 
+
   resources :posts
 end
