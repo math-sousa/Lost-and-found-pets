b04e795ca797d4c9b56381bf68d540d6202b963d
Luan Monteiro <luan.monteiro@spacesheepgames.com>
2022-10-18 13:43:54 -0300

    fix: changed unit tests to work with devise authentication

--- spec/models/perfil_spec.rb
+++ spec/models/perfil_spec.rb
@@ -3,51 +3,51 @@ require 'spec_helper'
 RSpec.describe Perfil, type: :model do
   it 'Invalido sem nome' do
     perfil = Perfil.new
-    perfil.email = "maria.silva@gmail.com"
-    perfil.telefone = "11999999999"
-    perfil.senha = "senhadamaria"
+    perfil.name = "maria.silva@gmail.com"
+    perfil.phone = "11999999999"
+    perfil.password = "senhadamaria"
     expect(perfil).not_to be_valid
   end
   
   it 'Invalido sem email' do
     perfil = Perfil.new
-    perfil.nome = "Maria Silva"
-    perfil.telefone = "11999999999"
-    perfil.senha = "senhadamaria"
+    perfil.name = "Maria Silva"
+    perfil.phone = "11999999999"
+    perfil.password = "senhadamaria"
     expect(perfil).not_to be_valid
   end
 
   it 'Invalido sem senha' do
     perfil = Perfil.new
     perfil.email = "maria.silva@gmail.com"
-    perfil.nome = "Maria Silva"
-    perfil.telefone = "11999999999"
+    perfil.name = "Maria Silva"
+    perfil.phone = "11999999999"
     expect(perfil).not_to be_valid
   end
 
   it 'Invalido sem telefone' do
     perfil = Perfil.new
     perfil.email = "maria.silva@gmail.com"
-    perfil.nome = "Maria Silva"
-    perfil.senha = "senhadamaria"
+    perfil.name = "Maria Silva"
+    perfil.password = "senhadamaria"
     expect(perfil).not_to be_valid
   end
 
   it 'Invalido com email com formato invalido' do
     perfil = Perfil.new
-    perfil.nome = "Maria Silva"
+    perfil.name = "Maria Silva"
     perfil.email = "1234%$$.com@"
-    perfil.telefone = "11999999999"
-    perfil.senha = "senhadamaria"
+    perfil.phone = "11999999999"
+    perfil.password = "senhadamaria"
     expect(perfil).not_to be_valid
   end
 
   it 'Invalido com telefone com formato invalido' do
     perfil = Perfil.new
-    perfil.nome = "Maria Silva"
+    perfil.name = "Maria Silva"
     perfil.email = "maria.silva@gmail.com"
-    perfil.telefone = "ABCDE"
-    perfil.senha = "senhadamaria"
+    perfil.phone = "ABCDE"
+    perfil.password = "senhadamaria"
     expect(perfil).not_to be_valid
   end
 end
