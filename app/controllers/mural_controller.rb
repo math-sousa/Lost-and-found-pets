262f4d13258e3060a22f2e927a9ed06b05103a70
Luan Monteiro <luan.monteiro@spacesheepgames.com>
2022-10-24 22:40:35 -0300

    feat: edited mural view and controller to support comment creation and visualization

--- app/controllers/mural_controller.rb
+++ app/controllers/mural_controller.rb
@@ -1,5 +1,17 @@
 class MuralController < ApplicationController
   def index
     @posts = Post.all
+    @comment = Comment.new
+  end
+
+  def create_comment
+    @comment = Post.find_by(id: comment_params[:post]).comments.create(content: comment_params[:content])
+    @posts = Post.all
+    render :index, status: :unprocessable_entity, content_type: "text/html"
+    headers["Content-Type"] = "text/html"
+  end
+
+  private def comment_params 
+    params.require(:comment).permit(:content, :post)
   end
 end
