577da716fcddcd4c98910dafbe30cafc6ebe5191
Raquel Almeida <raquelcristianealm@gmail.com>
2022-11-04 15:45:41 -0300

    fix: Edit profile error fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,14 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
 
-    protected
-    
-    # def user_update_params
-    #   accessible = [:name, :phone, :email]
-    #   params.require(:perfil).permit(accessible)
-    # end
-  
-    def update_resource(resource, params)
-      resource.update_without_password(params)<<[:name]
-    end
+  protected
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 
 end
