dd370179425d22614007aa70b601aa61d8d948e8
Luan Monteiro <luan.monteiro@spacesheepgames.com>
2022-11-09 11:31:49 -0300

    feat: add controller actions to edit posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,43 @@
 class PostsController < ApplicationController
+    before_action :authenticate_perfil!
+
     def new
         @post = Post.new
     end
+
     def create
-        @post = Post.new(post_params)
-        if @post.save
-            redirect_to @post
-        else
-            render :new, status: :unprocessable_entity, content_type: "text/html"
-            headers["Content-Type"] = "text/html"
+        if current_perfil
+            @post = current_perfil.posts.create(post_params.merge(perfil: current_perfil))
+            if @post.valid?
+                redirect_to @post
+            else
+                render :new, status: :unprocessable_entity, content_type: "text/html"
+                headers["Content-Type"] = "text/html"
+            end
         end
     end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        respond_to do |format|
+            if @post.update(post_params)
+                format.html { redirect_to post_url(@post), notice: "O post foi editado com sucesso." }
+                format.json { render :show, status: :ok, location: @classroom }
+            else
+                format.html { render :edit, status: :unprocessable_entity }
+                format.json { render json: @post.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
     def show
         @post = Post.find(params[:id])
     end
+
     def post_params 
         params.require(:post).permit(:titulo, :descricao, :tipo)
     end
