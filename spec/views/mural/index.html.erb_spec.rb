7ca3fdd548ed523dad7f1941a317600f911b3eab
Luan Monteiro <luan.monteiro@spacesheepgames.com>
2022-12-07 14:57:48 -0300

    test: add integration tests

--- spec/views/mural/index.html.erb_spec.rb
+++ spec/views/mural/index.html.erb_spec.rb
@@ -5,6 +5,59 @@ RSpec.describe "mural/index.html.erb", type: :view do
   include Devise::Test::IntegrationHelpers
   include Devise::Test::ControllerHelpers
 
+  it "does not render comments if the show comments button was not clicked" do
+    #log in
+    my_profile = create :perfil, :maria
+    sign_in my_profile 
+    
+    not_my_profile = create :perfil, :mario
+
+    #add post
+    post = create :post, perfil: my_profile
+    comment = create :comment, perfil_id: my_profile.id, post_id: post.id
+
+    @posts = Post.all
+    @comment = Comment.new
+
+    render
+
+    expect(rendered).to have_selector('.comment', visible: false)
+  end
+
+  it "render the show/hide comments link in post" do
+    #log in
+    my_profile = create :perfil, :maria
+    sign_in my_profile 
+    
+    not_my_profile = create :perfil, :mario
+
+    #add post
+    post = create :post, perfil: my_profile
+    comment = create :comment, perfil_id: my_profile.id, post_id: post.id
+    @posts = Post.all
+    @comment = Comment.new
+    render
+
+    expect(rendered).to have_content('Esconder/mostrar comentários')
+  end
+
+  it "do not render the show/hide comments link in post when there are no comment" do
+        #log in
+        my_profile = create :perfil, :maria
+        sign_in my_profile 
+        
+        not_my_profile = create :perfil, :mario
+    
+        #add post
+        post = create :post, perfil: my_profile
+        @posts = Post.all
+        @comment = Comment.new
+        render
+    
+        expect(rendered).to have_content('Este post não possui comentários!')
+        expect(rendered).to_not have_selector('.comment')
+  end
+
   it "render the edit button if post is yours" do
     #log in
     my_profile = create :perfil, :maria
@@ -18,8 +71,9 @@ RSpec.describe "mural/index.html.erb", type: :view do
     @comment = Comment.new
     render
 
-    rendered.should have_content('Editar post')
+    expect(rendered).to have_content('Editar post')
   end
+
   it "do not render the edit button if post is not yours" do
     #log in
     my_profile = create :perfil, :maria
@@ -33,7 +87,7 @@ RSpec.describe "mural/index.html.erb", type: :view do
     @comment = Comment.new
     render
 
-    rendered.should_not have_content('Editar post')
+    expect(rendered).to_not have_content('Editar post')
   end
 
   it "render the profile link in post" do
@@ -50,6 +104,6 @@ RSpec.describe "mural/index.html.erb", type: :view do
 
     render
 
-    rendered.should have_link(nil, href: "#{perfil_path(not_my_profile.id)}")
+    expect(rendered).to have_link(nil, href: "#{perfil_path(not_my_profile.id)}")
   end
 end
